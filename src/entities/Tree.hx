d6e061ed345fc4e9b71e9aa135f364553997ac24
Anders Nissen <andnis@gmail.com>
2015-12-12 15:33:10 +0100

    Update

--- src/entities/Tree.hx
+++ src/entities/Tree.hx
@@ -12,9 +12,11 @@ typedef Segment = {
 };
 
 class Tree extends Entity {
-    var numSegments :Int = 10;
+    var numSegments :Int = 8;
     var segments :Array<Segment>;
-    var segLength :Float = 30;
+    var segLength :Float = 40;
+    var max_width :Float = 20;
+    var min_width :Float = 1;
     var targetX :Float;
     var targetY :Float;
 
@@ -24,7 +26,6 @@ class Tree extends Entity {
         var lastSegment = segments[numSegments-1];
         lastSegment.x = 0;
         lastSegment.y = 0;
-        Luxe.draw.circle({ x: 200, y: -400, r: 80, color: new luxe.Color(0.8, 0.6, 0) });
     }
 
     override public function update(dt :Float) {
@@ -38,7 +39,7 @@ class Tree extends Entity {
             position_segment(segments[index], segments[index-1]);
         }
         for (i in 0 ... numSegments) {
-            draw_segment(segments[i], (i+1)*2);
+            draw_segment(segments[i], i);
         }
     }
 
@@ -55,18 +56,49 @@ class Tree extends Entity {
         targetY = yin - Math.sin(seg.angle) * segLength;
     }
 
-    function draw_segment(seg :Segment, sw :Float) {
+    function draw_segment(seg :Segment, i :Int) {
         var rot_x = seg.x + Math.cos(seg.angle) * segLength;
         var rot_y = seg.y + Math.sin(seg.angle) * segLength;
 
-        Luxe.draw.line({
-            p0: new Vector(seg.x, seg.y),
-            p1: new Vector(rot_x, rot_y),
-            color0: new Color(0.2, 0, 0.5),
-            color1: new Color(0.5, 0, 0.5),
+        var p0 = new Vector(seg.x, seg.y);
+        var p1 = new Vector(rot_x, rot_y);
+
+        var width_increase = (max_width - min_width) / numSegments;
+        // trace((i + 2) * width_increase);
+
+        var temp = Vector.Subtract(p1, p0).normalized;
+        var tangent = new Vector(-temp.y, temp.x);
+
+        var p2 = Vector.Multiply(tangent, min_width + (i + 1) * width_increase);
+        var p3 = Vector.Multiply(tangent, min_width + i * width_increase);
+        // var p2 = new Vector(-p0.y, p0.x);
+        // p2.normalize().multiplyScalar(min_width + (i + 1) * width_increase);
+        // var p3 = new Vector(-p1.y, p1.x);
+        // p3.normalize().multiplyScalar(min_width + i * width_increase);
+        Luxe.draw.circle({
+            x: seg.x,
+            y: seg.y,
+            r: min_width + (i + 1) * width_increase,
+            color: new Color(i / numSegments, 0.5 - (i / numSegments) * 0.5, 0.0), immediate: true
+        });
+
+        Luxe.draw.poly({
+            points: [
+                Vector.Add(p0, p2), Vector.Subtract(p0, p2),
+                Vector.Subtract(p1, p3), Vector.Add(p1, p3)
+            ],
+            color: new Color(i / numSegments, 0.5 - (i / numSegments) * 0.5, 0.0), //new Color(0.5, 0, 0.2),
             immediate: true
         });
 
-        Luxe.draw.circle({ x: seg.x, y: seg.y, r: 5, color: new luxe.Color(0.5, 0, 0.2), immediate: true });
+        // Luxe.draw.line({
+        //     p0: p0,
+        //     p1: p1,
+        //     color0: new Color(0.2, 0, 0.5),
+        //     color1: new Color(0.5, 0, 0.5),
+        //     immediate: true,
+        //     depth: 2
+        // });
+
     }
 }
--- src/states/Play.hx
+++ src/states/Play.hx
@@ -3,6 +3,7 @@ package states;
 import luxe.States;
 import luxe.Scene;
 import luxe.Vector;
+import luxe.Color;
 
 import entities.Tree;
 
@@ -22,7 +23,17 @@ class Play extends State {
 		lastStateScene = Luxe.scene;
 		Luxe.scene = stateScene;
 
+		Luxe.draw.circle({ x: 200, y: -400, r: 80, color: new luxe.Color(0.8, 0.6, 0) }); // sun
+
 		tree = new Tree();
+
+		Luxe.draw.box({
+			x: -Luxe.screen.width,
+			y: 0,
+			w: Luxe.screen.width * 2,
+			h: 200,
+			color: new Color(0.2, 0.6, 0.1)
+		});
 		// Luxe.draw.circle({ x: 100, y: 100, r: 100, color: new luxe.Color(1, 0, 0.5) });
 		// Luxe.draw.circle({ x: 120, y: 120, r: 40, color: new luxe.Color(0, 1, 0.5) });
 	}
